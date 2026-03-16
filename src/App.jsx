fd31bbbbcd4848cb63e8eea3f35096ccf693384d
Lahiru Sampath <lahirusampath0620@gmail.com>
2026-03-16 17:45:36 +0530

    Day 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import AdminPage from './pages/adminPage'
+import HomePage from './pages/homePage'
 import Trendingproducts from './components/trendingproducts'
 
 function App() {
   return (
-    <>
-      <div>
-        <Trendingproducts />
+
+      <div className='w-full h-screen border-[6px] border-black flex  items-center justify-center'  >
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          
+          <Route path='/admin' element={<AdminPage />} />
+        </Routes>
+       
+ 
       </div>
   
-    </>
+  
   )
 }
 
---
+++ src/pages/adminPage.jsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+    return(
+        <div className='w-full h-screen border-[6px] border-black flex  items-center justify-center'  >
+            <h1 className='text-5xl font-bold'>Welcome to the Admin Page</h1>
+        </div>
+    )
+}
